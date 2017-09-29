adba9f8c22fe48d0e9d719581fceb276068f039c
Pokechu22 <Pokechu022@gmail.com>
2017-09-29 16:50:14 -0700

    Compensate RDP display for MC's gui scaling and reduce duplicate vars

--- src/main/java/mcrdp/GuiRDPControl.java
+++ src/main/java/mcrdp/GuiRDPControl.java
@@ -10,6 +10,7 @@ import org.lwjgl.input.Mouse;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
@@ -26,10 +27,19 @@ public class GuiRDPControl extends GuiScreen {
 	private static final int WINDOW_LOWER_U = WINDOW_TEX_WIDTH - WINDOW_EDGE_SIZE;
 	private static final int WINDOW_LOWER_V = WINDOW_TEX_HEIGHT - WINDOW_EDGE_SIZE;
 
-	// Border (window) values
-	private int windowWidth, windowHeight, windowX, windowY;
-	// Display info
+	/**
+	 * Minimum resolution guaranteed given to the RDP screen.
+	 */
+	private static final int GUARANTEED_WIDTH = 96, GUARANTEED_HEIGHT = 72;
+
+	// Display position info
 	private int dispWidth, dispHeight, dispX, dispY;
+
+	/**
+	 * Width of a single gui pixel on MC (or, the ratio between display width and
+	 * actual GUI width)
+	 */
+	private int mcScaleFactor;
 
 	/**
 	 * True if the RDP screen is currently focused.
@@ -59,29 +69,32 @@ public class GuiRDPControl extends GuiScreen {
 	public void initGui() {
 		addButton(new GuiButton(0, width / 2 - 200 / 2, height - 40, I18n.format("gui.done")));
 
-		windowWidth = Math.max(Math.min(instance.width + 18, this.width - 200), 100);
-		windowHeight = Math.max(Math.min(instance.height + 27, this.height - 80), 100);
-		windowX = this.width / 2 - windowWidth / 2;
-		windowY = 20;
-		dispWidth = windowWidth - 18;
-		dispHeight = windowHeight - 27;
-		dispX = windowX + 9;
-		dispY = windowY + 18;
+		mcScaleFactor = new ScaledResolution(this.mc).getScaleFactor();
+
+		dispWidth = Math.max(Math.min(instance.width / mcScaleFactor, this.width - 200), GUARANTEED_WIDTH);
+		dispHeight = Math.max(Math.min(instance.height / mcScaleFactor, this.height - 100), GUARANTEED_HEIGHT);
+		dispX = this.width / 2 - dispWidth / 2;
+		dispY = 38;
 	}
 
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		drawDefaultBackground();
 
+		int windowWidth = dispWidth + 18;
+		int windowHeight = dispHeight + 27;
+		int windowX = dispX - 9;
+		int windowY = dispY - 18;
+
 		GlStateManager.enableBlend();
 
 		mc.getTextureManager().bindTexture(WINDOW);
-		int x = mouseX - dispX;
-		int y = mouseY - dispY;
+		int x = (mouseX - dispX) * mcScaleFactor;
+		int y = (mouseY - dispY) * mcScaleFactor;
 		if (mouseButtons == 0) {
 			// Only re-evaluate focus when there are no mouse buttons held (to allow
 			// dragging out of bounds) 
-			this.rdpFocused = (x >= 0 && x < dispWidth && y >= 0 && y < dispHeight);
+			this.rdpFocused = (x >= 0 && x < dispWidth * mcScaleFactor && y >= 0 && y < dispHeight * mcScaleFactor);
 		}
 		if (rdpFocused) {
 			GlStateManager.color(.75f, .75f, .75f);
@@ -97,7 +110,11 @@ public class GuiRDPControl extends GuiScreen {
 
 		GlStateManager.color(1, 1, 1);
 		GlStateManager.bindTexture(instance.glId);
-		drawModalRectWithCustomSizedTexture(dispX, dispY, 0, 0, dispWidth, dispHeight, instance.width, instance.height);
+
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(1.0/mcScaleFactor, 1.0/mcScaleFactor, 1);
+		drawModalRectWithCustomSizedTexture(dispX * mcScaleFactor, dispY * mcScaleFactor, 0, 0, dispWidth * mcScaleFactor, dispHeight * mcScaleFactor, instance.width, instance.height);
+		GlStateManager.popMatrix();
 
 		this.fontRenderer.drawString("MCRDP - " + instance.server, windowX + 8, windowY + 6, 0x404040);
 
@@ -126,8 +143,8 @@ public class GuiRDPControl extends GuiScreen {
 
 		mouseButtons |= 1 << mouseButton;
 
-		int x = mouseX - dispX;
-		int y = mouseY - dispY;
+		int x = (mouseX - dispX) * mcScaleFactor;
+		int y = (mouseY - dispY) * mcScaleFactor;
 		if (rdpFocused) {
 			if (instance.input.canSendButton(mouseButton + 1)) {
 				instance.input.mouseButton(mouseButton + 1, true, x, y);
@@ -158,8 +175,8 @@ public class GuiRDPControl extends GuiScreen {
 		mouseButtons &= ~(1 << state);
 
 		if (rdpFocused) {
-			int x = mouseX - dispX;
-			int y = mouseY - dispY;
+			int x = (mouseX - dispX) * mcScaleFactor;
+			int y = (mouseY - dispY) * mcScaleFactor;
 			if (instance.input.canSendButton(state + 1)) {
 				instance.input.mouseButton(state + 1, false, x, y);
 			}
